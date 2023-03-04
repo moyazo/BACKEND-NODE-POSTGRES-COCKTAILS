6f66aa07987b3ae9bb6bad0151ef8d302cd54527
Manuel Moya <manuel.moyavadillo2@gmail.com>
2023-03-04 12:40:46 +0100

    feat:get categories

--- seeders/20230304100246-PostSeeder.js
+++ seeders/20230304100246-PostSeeder.js
@@ -1,19 +1,37 @@
 'use strict';
 const uuid = require('uuid');
-
+const faker = require('faker');
+const db = require('../src/models/index.js');
+const Category = db.Category;
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
     async up(queryInterface, Sequelize) {
-        const postToCreate = [];
-        for (var i = 0; i < 40; i++) {
+        let postToCreate = [];
+        let categoriesToCreate = [];
+        const Categories = await Category.findAll();
+        Categories.forEach((category) => {
+            categoriesToCreate.push(category);
+        });
+        const images = [
+            'https://www.tastingtable.com/img/gallery/11-cocktails-to-try-if-you-like-drinking-gin/intro-1659025591.jpg',
+            'https://thumbs.dreamstime.com/b/set-various-cocktails-black-background-set-various-cocktails-shaker-black-background-188649840.jpg',
+            'https://hips.hearstapps.com/del.h-cdn.co/assets/15/24/1434133241-milkshakes.jpg',
+            'https://japanese-clothing.com/wp-content/uploads/2022/06/Sans_titre_-_2020-05-17T154130.995_1.jpg?v=1589722950',
+            'https://recipe4appetite.com/img/recipe-img/homemade-cocoa-drink.jpg',
+            'https://149694725.v2.pressablecdn.com/wp-content/uploads/2018/03/Popular-Shot-Recipes.jpg',
+            'https://www.getflavor.com/wp-content/uploads/2021/09/Latte-and-Tea-Mojito-1152.jpg',
+            'https://images.immediate.co.uk/production/volatile/sites/30/2022/08/200615BBCGOODFOODCOVERVEGXMASCOVER202004592-copy-95936cf.jpg',
+            'https://petersfoodadventures.com/wp-content/uploads/2018/12/christmas-punch-1.png',
+            'https://cdn2.justwineapp.com/assets/article/2017/01/Bier-Cocktail-Pale-Ale-darkrye-com.jpg',
+            'https://dosaandcurry.ca/wp-content/uploads/2021/11/Soft-Drinks.jpg',
+        ];
+        for (var i = 0; i < 11; i++) {
             postToCreate.push({
                 id: uuid.v4(),
-                title: faker.name.first(),
-                email: faker.internet.email(),
-                phone: faker.phone.phoneNumber(),
-                ip: faker.internet.ip(),
-                address: faker.address.streetAddress(),
-                city: faker.address.cityName(),
+                title: faker.name.firstName(),
+                category: categoriesToCreate[i],
+                image: images[i],
+                comment: faker.lorem.paragraph(),
                 createdAt: new Date(),
                 updatedAt: new Date(),
             });
--- src/routes/category.js
+++ src/routes/category.js
@@ -1,11 +1,28 @@
 const Router = require('express').Router;
 const { apiCallByCategory } = require('../services/cocktailApi');
+const { getCategories } = require('../controllers/category');
+const {
+    getCocktailList,
+    getSubCocktailById,
+} = require('../controllers/subCocktail');
 const routerCategoryApi = Router();
 
 routerCategoryApi.get('/sync', async (req, res) => {
     try {
         await apiCallByCategory();
         res.status(200).json('Data synchronize successfully');
+    } catch (error) {
+        console.log(error);
+        res.status(500).json('No new documents found' + error.message);
+    }
+});
+routerCategoryApi.get('/', async (req, res) => {
+    try {
+        const categories = await getCategories();
+        if (!categories) {
+            res.status(403).json('NOT CATEGORIES FOUND');
+        }
+        res.status(200).json(categories);
     } catch (error) {
         console.log(error);
         res.status(500).json('No new documents found' + error.message);
